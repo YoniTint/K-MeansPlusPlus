a86adfe83630e19bd65b3754f6aa92dec70784c1
Yonatan Tin <yonitint1@gmail.com>
2020-12-24 14:50:39 +0200

    reduced time complexity of py file tremendously

--- kmeans_pp.py
+++ kmeans_pp.py
@@ -9,20 +9,16 @@ def k_means_pp(K, N, d, MAX_ITER, observations):
 
     centroids = np.zeros(shape=(K, d), dtype=float)
     centroids_indexes = np.zeros(shape=K, dtype=int)
-    min_distances = np.zeros(shape=N, dtype=float)
 
     first_chosen_centroid_index = np.random.choice(N)
     centroids[0] = observations[first_chosen_centroid_index]
     centroids_indexes[0] = first_chosen_centroid_index
 
-    for i in range(0, N):
-        min_distances[i] = np.linalg.norm(observations[i] - centroids[0]) ** 2
+    min_distances = np.power(observations - centroids[0], 2).sum(axis=1)
 
     for j in range(1, K):
-        for i in range(0, N):
-            curr_distance_squared = np.linalg.norm(observations[i] - centroids[j - 1]) ** 2
-            if curr_distance_squared < min_distances[i]:
-                min_distances[i] = curr_distance_squared
+        curr_min_distances = np.power(observations - centroids[j - 1], 2).sum(axis=1)
+        min_distances = np.minimum(min_distances, curr_min_distances)
 
         omega = np.sum(min_distances)
         prob_array = np.divide(min_distances, omega)
@@ -59,3 +55,4 @@ table = pd.read_csv(filename, header=None)
 table = table.to_numpy()
 
 k_means_pp(K, N, d, MAX_ITER, table)
+
